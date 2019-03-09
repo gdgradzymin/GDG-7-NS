6ec63dd5735bcc4d350c3a2627be3dd253653b0a
Sebastian Denis <sebastian.denis@outlook.com>
2019-03-09 22:21:41 +0100

    refactoring

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,45 +1,40 @@
-import { ItemEventData } from "tns-core-modules/ui/list-view"
+import { ItemEventData } from "tns-core-modules/ui/list-view";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-    selector: "Home",
-    moduleId: module.id,
-    templateUrl: "./home.component.html",
-    styleUrls: ['./home.component.css']
+  selector: "Home",
+  moduleId: module.id,
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  clickCount: number = 0;
+  txt: string = "";
 
-    clickCount: number = 0;
-    clickCountStr: string = "";
-    onItemTap(args: ItemEventData): void {
-        console.log('Item with index: ' + args.index + ' tapped');
-        
-        var res = localStorage.getItem('counter');
-        if(res != null){
-            this.clickCount = parseInt(res);
-            this.clickCount = this.clickCount + 1;
-        } else{
-            this.clickCount = 1;
-        }
-        console.log(this.clickCount);
-        localStorage.setItem('counter', this.clickCount.toString());
+  lista: string[] = ["pozycja 1", "pozycja 2"];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const res = localStorage.getItem("counter");
+    if (res) {
+      this.clickCount = parseInt(res);
     }
-    txt: string = "";
+  }
 
-    lista: string[] = ["pozycja 1", "pozycja 2"];
-
-    dodaj(): void {
-        if (this.txt) {
-            this.lista.push(this.txt);
-            this.txt = "";
-        }
-
+  dodaj(): void {
+    if (this.txt) {
+      this.lista.push(this.txt);
+      this.txt = "";
     }
+  }
 
+  onItemTap(args: ItemEventData): void {
+    // console.log("Item with index: " + args.index + " tapped");
 
-    constructor() {
-    }
+    this.clickCount++;
 
-    ngOnInit(): void {
-    }
+    // console.log(this.clickCount);
+    localStorage.setItem("counter", this.clickCount.toString());
+  }
 }
