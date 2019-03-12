63ad90d36cb82ee855c073a9722c68ccf43f6225
Eyzo280 <48450982+Eyzo280@users.noreply.github.com>
2019-03-12 09:42:55 +0100

    LocalStorage

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -12,8 +12,7 @@ export class HomeComponent implements OnInit {
 
     clickCount: number = 0;
     txt: string = "";
-
-    lista: string[] = ["pozycja 1", "pozycja 2"];
+    lista: string[] = [];
 
     constructor() { }
 
@@ -22,24 +21,31 @@ export class HomeComponent implements OnInit {
         if (res) {
             this.clickCount = parseInt(res);
         }
+        const zmienne = localStorage.getItem("wartosci");
+        if (zmienne) {
+            this.lista = zmienne.split(',');
+        }
     }
 
     dodaj(): void {
         if (this.txt) {
             this.lista.push(this.txt);
             this.txt = "";
+            localStorage.setItem("wartosci", this.lista.toString());
         }
     }
 
     remove(): void {
         if (this.lista.length > 0) {
             this.lista.pop();
+            localStorage.setItem("wartosci", this.lista.toString());
         }
     }
 
     removeAll(): void {
         if (this.lista.length > 0) {
             this.lista.length = 0;
+            localStorage.setItem("wartosci", this.lista.toString());
         }
     }
 
